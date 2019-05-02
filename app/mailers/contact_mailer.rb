d0654688d932c395035e39be0ea543e385fc61cd
K_18194 <fastofall6545@gmail.com>
2019-05-02 16:27:01 +0000

    Heroku Production deploy

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
     def contact_mail(contact)
      @contact = contact
-     mail to: '0501test@gmail.com', subject: "お問い合わせの確認メール"
+     mail to: 'fastofall6545@gmail.com', subject: "お問い合わせの確認メール"
     end
 end
